82b009182ac0b5fa09fd5dc19f82b9c16e966af6
dev <81720436+developer9998@users.noreply.github.com>
2023-12-23 16:29:03 -0330

    Initial commit

---
+++ NekoLongChair/LongChairMod.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+using NekoLongChair;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[assembly: MelonInfo(typeof(LongChairMod), "NekoLongChair", "1.0.0", "dev")]
+
+namespace NekoLongChair
+{
+    public class LongChairMod : MelonMod
+    {
+        private Transform m_RootEnv, m_Chair;
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            if (sceneName == "SceneSub")
+            {
+                Scene activeScene = SceneManager.GetSceneByBuildIndex(buildIndex);
+
+                foreach (GameObject rootObject in activeScene.GetRootGameObjects())
+                {
+                    if (rootObject.name == "Environment")
+                    {
+                        m_RootEnv = rootObject.transform;
+                        m_Chair = m_RootEnv.Find("TestBG01_Niwa24_Fix_pref/Bone_Chair/Bone");
+                        m_Chair.localScale = new Vector3(4f, 1f, 1f);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# NekoLongChair
+ A mod for "Neko Atsume Purrfect Kitty Collector" which modifies the existing chair in the game to be of a longer scale.
